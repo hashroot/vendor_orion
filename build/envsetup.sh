66b66810ef88b28ea62e38f0435ffc732bf0c17a
cybojenix <anthonydking@gmail.com>
2015-09-07 16:02:10 -0400

    Merge changes Iae29d0c8,Ia37364e4,I6e0f8f77 into lp5.1 * changes:   slim: envsetup: force all variables to be local   slim: envsetup: add an append method for hmm   slim: envsetup: add new entries for hmm Change-Id: Ib306ec8cc67e471e57fc17aad5c93f9a569c0110

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,7 +1,7 @@
 # Orion functions that extend build/envsetup.sh
 
 function orion_device_combos() {
-    local device
+     local T list_file variant device
 
     T="$(gettop)"
     list_file="${T}/vendor/orion/orion.devices"
@@ -40,27 +40,33 @@ function orion_rename_function() {
     eval "original_orion_$(declare -f ${1})"
 }
 
-function orion_add_hmm_entry() {
-    f_name="${1}"
-    f_desc="${2}"
+function _orion_build_hmm() #hidden
+{
+    printf "%-8s %s" "${1}:" "${2}"
+}
 
-    function _build_entry() {
-        printf "%-8s %s" "${f_name}:" "${f_desc}"
-    }
+function orion_append_hmm()
+{
+    HMM_DESCRIPTIVE=("${HMM_DESCRIPTIVE[@]}" "$(_orion_build_hmm "$1" "$2")")
+}
 
+function orion_add_hmm_entry()
+{
     for c in ${!HMM_DESCRIPTIVE[*]}
     do
-        if [[ "${f_name}" == $(echo "${HMM_DESCRIPTIVE[$c]}" | cut -f1 -d":") ]]
+        if [[ "${1}" == $(echo "${HMM_DESCRIPTIVE[$c]}" | cut -f1 -d":") ]]
         then
-            HMM_DESCRIPTIVE[${c}]="$(_build_entry)"
+            HMM_DESCRIPTIVE[${c}]="$(_slim_build_hmm "$1" "$2")"
             return
         fi
     done
-    HMM_DESCRIPTIVE=(HMM_DESCRIPTIVE[@] "$(_build_entry)")
+    orion_append_hmm "$1" "$2"
 }
 
 function orionremote()
 {
+	local proj pfx project
+
     if ! git rev-parse &> /dev/null
     then
         echo "Not in a git directory. Please run this from an Android repository you wish to set up."
@@ -82,6 +88,8 @@ function orionremote()
 
 function cmremote()
 {
+	local proj pfx project
+
     if ! git rev-parse &> /dev/null
     then
         echo "Not in a git directory. Please run this from an Android repository you wish to set up."
@@ -98,6 +106,8 @@ function cmremote()
 
 function aospremote()
 {
+	local proj pfx project
+
     if ! git rev-parse &> /dev/null
     then
         echo "Not in a git directory. Please run this from an Android repository you wish to set up."
@@ -116,6 +126,8 @@ function aospremote()
 
 function cafremote()
 {
+	local proj pfx project
+
     if ! git rev-parse &> /dev/null
     then
         echo "Not in a git directory. Please run this from an Android repository you wish to set up."
@@ -133,6 +145,7 @@ function cafremote()
 
 function orion_push()
 {
+    local branch ssh_name path_opt proj
     branch="lp5.1"
     ssh_name="orion_review"
     path_opt=
@@ -156,16 +169,22 @@ function orion_push()
 }
 
 
-slim_rename_function hmm
+orion_rename_function hmm
 function hmm() #hidden
 {
+	local i T
     T="$(gettop)"
-    original_slim_hmm
+    original_orion_hmm
     echo
 
-    echo "vendor/slim extended functions. The complete list is:"
-    for i in $(grep -P '^function .*$' "$T/vendor/slim/build/envsetup.sh" | grep -v "#hidden" | sed 's/function \([a-z_]*\).*/\1/' | sort | uniq); do
+    echo "vendor/orion extended functions. The complete list is:"
+    for i in $(grep -P '^function .*$' "$T/vendor/orion/build/envsetup.sh" | grep -v "#hidden" | sed 's/function \([a-z_]*\).*/\1/' | sort | uniq); do
         echo "$i"
     done |column
 }
+
+orion_append_hmm "slimremote" "Add a git remote for matching slim repository"
+orion_append_hmm "cmremote" "Add a git remote for matching CM repository"
+orion_append_hmm "aospremote" "Add git remote for matching AOSP repository"
+orion_append_hmm "cafremote" "Add git remote for matching CodeAurora repository."
 
